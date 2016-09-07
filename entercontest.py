f67d1c0a3b4bc442b23379b4ff84c99da87c2d7d
Benjamin Henshall <ben@henshall.plus.com>
2016-09-07 20:22:42 +0100

    Made faster and fixed

--- entercontest.py
+++ entercontest.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 import time
 
 user_agent = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) " + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36")
@@ -16,10 +13,10 @@ for points in NUM_OF_POINTS_TO_STOP_ON:
 	winningurl = "http://www.papajohns.co.uk/paparewards/images/reveal-%s-points.gif" %points
 	WINNING_URLS.append(winningurl)
 
-EMAILNAME = "email"
-EMAILPROVIDER = "@email.com"
+EMAILNAME = "ilikespam"
+EMAILPROVIDER = "@hotmail.co.uk"
 
-NUM_TO_START_AT = 70
+NUM_TO_START_AT = 175
 i = NUM_TO_START_AT
 
 won = False
@@ -27,32 +24,51 @@ while not won:
 	
 	#while not won:
 	driver = webdriver.Chrome()
-	driver.set_window_size(600, 600)
+	driver.set_window_size(1200, 600)
 
 	emailstring = "%s+%i%s" %(EMAILNAME, i, EMAILPROVIDER)
 
-	print("Attempting to enter contest with %s." % emailstring)
-
 	driver.get(URL)
+	
+	loaded = False
+	while not loaded:
+		if driver.current_url == URL:
+			loaded = True
+			time.sleep(2)
+		else:
+			print "trying to load first page"
+			time.sleep(2)
+			
+	print("Attempting to enter contest with %s." % emailstring)
+	
 	elem = driver.find_element_by_name("ctl00$cphBody$txtEmailAddressEnter")
 	elem.send_keys(emailstring)
 
 	elem = driver.find_element_by_id("ctl00_cphBody_btnNext")
 	elem.click()
-
+	print "trying to load second page"
 	loaded = False
 	while not loaded:
 		newurl = str(driver.current_url).split("?")
 		if newurl[0] == "http://www.papajohns.co.uk/paparewards/play.aspx":
 			loaded = True
 			print "loaded page, waiting for wheel to spin"
-			time.sleep(10)
 		else:
-			print "trying to load page"
 			time.sleep(2)
+	
+	print "Waiting for wheel to spin..."
+	
+	wheelspan = False
+	while not wheelspan:
+		elem = driver.find_element_by_id("wheel")
+		print elem.get_attribute('src')
+		if elem.get_attribute('src') == "http://www.papajohns.co.uk/paparewards/images/full-wheel.gif":
+			time.sleep(2)
+		else:
+			print "Wheel span, continuing"
+			wheelspan = True
 
-	elem = driver.find_element_by_id("wheel")
-
+	print(elem.get_attribute('src'))		
 	if elem.get_attribute('src') in WINNING_URLS:
 		won = True
 		print "YOU WON! FILL OUT YOUR DETAILS TO COLLECT!"
